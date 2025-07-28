61a30caa7df7e7e361373eab27aaaca99c3ee433
krishna <208x1a0538@khitguntur.ac.in>
2025-07-28 11:42:28 +0530

    Find Nth Root Of Element -> Binary Search

---
+++ BinarySearch/BinarySearchAnswers/FindNthRoot.java
@@ -0,0 +1,15 @@
+/*
+    This is solved using binart=y search and i used BS because iam checking in the range from 0 to given num m which is sorted
+ */
+public class Solution {
+    public static int findNthRoot(int n, int m, int l,int h){
+        if(l > h) return -1;
+        int mid = l + (h - l)/2;
+        if(Math.pow(mid, n) == m) return mid;
+        else if(Math.pow(mid, n) > m) return findNthRoot(n, m, l, mid - 1);
+        else return findNthRoot(n, m, mid + 1, h);
+    }
+    public static int NthRoot(int n, int m) {
+        return findNthRoot(n, m, 0, m);
+    }
+}
